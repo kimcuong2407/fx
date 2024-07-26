8041120ad26a8cab2e9ea555b56e5cc7980fbfe2
Cuongnk <cuong@swiftpod.com>
2024-07-26 15:54:22 +0700

    course

--- websocket.py
+++
@@ -1,9 +0,0 @@
-from websocket import WebSocketApp
-
-
-def on_message(wsapp, message):
-    print(message)
-
-
-wsapp = WebSocketApp("wss://stream.binance.com:9443/ws/block", on_message=on_message)
-wsapp.run_forever()
